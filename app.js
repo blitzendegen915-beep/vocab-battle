a96d731516fbdc413c5db91cd0af706c82e6d76d
blitzendegen915 <blitzendegen915@gmail.com>
2026-05-22 22:10:35 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const STORAGE = {
 const ADMIN_SESSION_KEY = "vocabBattleAdminUnlocked";
 const ADMIN_PIN_SESSION_KEY = "vocabBattleAdminPin";
 const ADMIN_PASSWORD_HASH = "75ae5d65da5fbbbcaf62828269c71b049d88755196f6fab97dd3a04a6720fd92";
-const DEFAULT_GAS_URL = "https://script.google.com/macros/s/AKfycbw4wucQB8S-zT530pAJk1ogBWfHBQ4XBb86lebV8yuLCIRghx88Wt4IunD07fAEcgeE/exec";
+const DEFAULT_GAS_URL = "https://script.google.com/macros/s/AKfycbytVz4FsKrCy1160KkpnvksFiluhOW8EtQQtppF1SW1S3X_9-Ki05AjSaoylhro06ti/exec";
 
 const sampleWords = [
   { word: "important", meaning: "重要な", difficulty: 6, unit: "Sample", enabled: true },
